7d4f940ca69602b9a44bd34fcf23c261e3f0bdbc
Stefan Reinauer <stefan.reinauer@coreboot.org>
2015-08-18 12:03:34 -0700

    Move SPI Trace related operations into separate section
    Change-Id: I90780fdef5ea7c0801f69e110bd3fda5948d5bfc
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/11266
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- em100.c
+++ em100.c
@@ -71,99 +71,6 @@ static int get_response(libusb_device_handle *dev, void *data, int length)
 	return actual;
 }
 
-/**
- * reset_spi_trace: clear SPI trace buffer
- * @param em100: em100 device structure
- *
- * out(16 bytes): 0xbd 0 .. 0
- */
-static int reset_spi_trace(struct em100 *em100)
-{
-	unsigned char cmd[16];
-	memset(cmd, 0, 16);
-	cmd[0] = 0xbd; /* reset SPI trace buffer*/
-	if (!send_cmd(em100->dev, cmd)) {
-		return 0;
-	}
-	return 1;
-}
-
-/**
- * read_spi_trace: fetch SPI trace data
- * @param em100: em100 device structure
- * globals: curpos, counter, cmdid
- *
- * out(16 bytes): bc 00 00 00 08 00 00 00 00 15 00 00 00 00 00 00
- * in(8x8192 bytes): 2 bytes (BE) number of records (0..0x3ff),
- *    then records of 8 bytes each
- */
-unsigned int counter = 0;
-unsigned char curpos = 0;
-unsigned char cmdid = 0xff; // timestamp, so never a valid command id
-static int read_spi_trace(struct em100 *em100)
-{
-	unsigned char cmd[16];
-	unsigned char data[8192];
-	unsigned int count, i, report;
-	memset(cmd, 0, 16);
-	cmd[0] = 0xbc; /* read SPI trace buffer*/
-	cmd[4] = 0x08; /* cmd1..cmd4 are probably u32BE on how many
-			  reports (8192 bytes each) to fetch */
-	cmd[9] = 0x15; /* no idea */
-	if (!send_cmd(em100->dev, cmd)) {
-		printf("sending trace command failed\n");
-		return 0;
-	}
-	for (report = 0; report < 8; report++) {
-		memset(data, 0, sizeof(data));
-		int len = get_response(em100->dev, data, sizeof(data));
-		if (len != sizeof(data)) {
-			/* FIXME: handle error: device reset? */
-			printf("error, len = %d instead of %zd. bailing out\n\n", len, sizeof(data));
-			return 0;
-		}
-		count = (data[0] << 8) | data[1];
-		for (i = 0; i < count; i++) {
-			unsigned int j;
-			unsigned char cmd = data[2 + i*8];
-			if (cmd == 0xff) {
-				/* timestamp */
-				unsigned long long timestamp = 0;
-				timestamp = data[2 + i*8 + 2];
-				timestamp = (timestamp << 8) | data[2 + i*8 + 3];
-				timestamp = (timestamp << 8) | data[2 + i*8 + 4];
-				timestamp = (timestamp << 8) | data[2 + i*8 + 5];
-				timestamp = (timestamp << 8) | data[2 + i*8 + 6];
-				timestamp = (timestamp << 8) | data[2 + i*8 + 7];
-				printf("\ntimestamp: %lld.%lld", timestamp / 100000000, timestamp % 100000000);
-				continue;
-			}
-#if 0
-			printf("{(%d)", curpos);
-			for (j = 0; j < 8; j++) {
-				printf("%02x ", data[2 + i*8 + j]);
-			}
-			printf("}");
-#endif
-			/* from here, it must be data */
-			if (cmd != cmdid) {
-				/* new command */
-				cmdid = cmd;
-				printf("\nspi command %6d: ", ++counter);
-				curpos = 0;
-			}
-			/* this exploits 8bit wrap around in curpos */
-			unsigned char blocklen = (data[2 + i*8 + 1] - curpos);
-			blocklen /= 8;
-			for (j = 0; j < blocklen; j++) {
-				printf("%02x ", data[2 + i*8 + 2 + j]);
-			}
-			curpos = data[2 + i*8 + 1] + 0x10; // this is because the em100 counts funny
-		}
-	}
-	return 1;
-}
-
 /* System level operations */
 
 /**
@@ -749,6 +656,101 @@ static int write_ht_register(struct em100 *em100, int reg, uint8_t val)
 	cmd[2] = val;
 	if (!send_cmd(em100->dev, cmd)) {
 		return 0;
+	}
+	return 1;
+}
+
+/* SPI Trace related operations */
+
+/**
+ * reset_spi_trace: clear SPI trace buffer
+ * @param em100: em100 device structure
+ *
+ * out(16 bytes): 0xbd 0 .. 0
+ */
+static int reset_spi_trace(struct em100 *em100)
+{
+	unsigned char cmd[16];
+	memset(cmd, 0, 16);
+	cmd[0] = 0xbd; /* reset SPI trace buffer*/
+	if (!send_cmd(em100->dev, cmd)) {
+		return 0;
+	}
+	return 1;
+}
+
+/**
+ * read_spi_trace: fetch SPI trace data
+ * @param em100: em100 device structure
+ * globals: curpos, counter, cmdid
+ *
+ * out(16 bytes): bc 00 00 00 08 00 00 00 00 15 00 00 00 00 00 00
+ * in(8x8192 bytes): 2 bytes (BE) number of records (0..0x3ff),
+ *    then records of 8 bytes each
+ */
+unsigned int counter = 0;
+unsigned char curpos = 0;
+unsigned char cmdid = 0xff; // timestamp, so never a valid command id
+static int read_spi_trace(struct em100 *em100)
+{
+	unsigned char cmd[16];
+	unsigned char data[8192];
+	unsigned int count, i, report;
+	memset(cmd, 0, 16);
+	cmd[0] = 0xbc; /* read SPI trace buffer*/
+	cmd[4] = 0x08; /* cmd1..cmd4 are probably u32BE on how many
+			  reports (8192 bytes each) to fetch */
+	cmd[9] = 0x15; /* no idea */
+	if (!send_cmd(em100->dev, cmd)) {
+		printf("sending trace command failed\n");
+		return 0;
+	}
+	for (report = 0; report < 8; report++) {
+		memset(data, 0, sizeof(data));
+		int len = get_response(em100->dev, data, sizeof(data));
+		if (len != sizeof(data)) {
+			/* FIXME: handle error: device reset? */
+			printf("error, len = %d instead of %zd. bailing out\n\n", len, sizeof(data));
+			return 0;
+		}
+		count = (data[0] << 8) | data[1];
+		for (i = 0; i < count; i++) {
+			unsigned int j;
+			unsigned char cmd = data[2 + i*8];
+			if (cmd == 0xff) {
+				/* timestamp */
+				unsigned long long timestamp = 0;
+				timestamp = data[2 + i*8 + 2];
+				timestamp = (timestamp << 8) | data[2 + i*8 + 3];
+				timestamp = (timestamp << 8) | data[2 + i*8 + 4];
+				timestamp = (timestamp << 8) | data[2 + i*8 + 5];
+				timestamp = (timestamp << 8) | data[2 + i*8 + 6];
+				timestamp = (timestamp << 8) | data[2 + i*8 + 7];
+				printf("\ntimestamp: %lld.%lld", timestamp / 100000000, timestamp % 100000000);
+				continue;
+			}
+#if 0
+			printf("{(%d)", curpos);
+			for (j = 0; j < 8; j++) {
+				printf("%02x ", data[2 + i*8 + j]);
+			}
+			printf("}");
+#endif
+			/* from here, it must be data */
+			if (cmd != cmdid) {
+				/* new command */
+				cmdid = cmd;
+				printf("\nspi command %6d: ", ++counter);
+				curpos = 0;
+			}
+			/* this exploits 8bit wrap around in curpos */
+			unsigned char blocklen = (data[2 + i*8 + 1] - curpos);
+			blocklen /= 8;
+			for (j = 0; j < blocklen; j++) {
+				printf("%02x ", data[2 + i*8 + 2 + j]);
+			}
+			curpos = data[2 + i*8 + 1] + 0x10; // this is because the em100 counts funny
+		}
 	}
 	return 1;
 }
