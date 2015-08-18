dab960ecb04c5465cb573fe7468ed9ff56851411
Stefan Reinauer <stefan.reinauer@coreboot.org>
2015-08-18 10:55:26 -0700

    Add SPI flash related functions
    Change-Id: If02ceaa04abe69ca2fe3759804987a6d66b946d2
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/11260
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- em100.c
+++ em100.c
@@ -370,6 +370,127 @@ static int write_fpga_register(struct em100 *em100, int reg, int val)
 	return 1;
 }
 
+/* SPI flash related operations */
+
+static int get_spi_flash_id(struct em100 *em100)
+{
+	unsigned char cmd[16];
+	unsigned char data[512];
+	memset(cmd, 0, 16);
+	cmd[0] = 0x30; /* Get SPI flash ID */
+	if (!send_cmd(em100->dev, cmd)) {
+		return 0;
+	}
+	int len = get_response(em100->dev, data, 512);
+	if (len == 3) {
+		int id = (data[0] << 16) | (data[1] << 8) | data[2];
+		return id;
+	}
+	return 0;
+}
+
+static int erase_spi_flash(struct em100 *em100)
+{
+	unsigned char cmd[16];
+	memset(cmd, 0, 16);
+	cmd[0] = 0x31; /* Erase SPI flash */
+	if (!send_cmd(em100->dev, cmd)) {
+		return 0;
+	}
+	/* Specification says to wait 5s before
+	 * issuing another USB command
+	 */
+	sleep(5);
+	return 1;
+}
+
+static int poll_spi_flash_status(struct em100 *em100)
+{
+	unsigned char cmd[16];
+	unsigned char data[1];
+	memset(cmd, 0, 16);
+	cmd[0] = 0x32; /* Poll SPI flash status */
+	if (!send_cmd(em100->dev, cmd)) {
+		return 0;
+	}
+	int len = get_response(em100->dev, data, 1);
+	if ((len == 1) && (data[0] == 1)) {
+		/* ready */
+		return 1;
+	}
+	/* busy (or read unsuccessful) */
+	return 0;
+}
+
+/**
+ * read_spi_flash_page: fetch SPI flash page
+ * @param em100: initialized em100 device structure
+ *
+ * out(16 bytes): 0x33 addr addr addr .. 0
+ * in(len + 255 bytes): 0xff ?? serno_lo serno_hi ?? ?? .. ??
+ */
+static int read_spi_flash_page(struct em100 *em100, int addr, unsigned char *blk)
+{
+	unsigned char cmd[16];
+	unsigned char data[256];
+	memset(cmd, 0, 16);
+	cmd[0] = 0x33; /* read configuration block */
+	cmd[1] = (addr >> 16) & 0xff;
+	cmd[2] = (addr >> 8)  & 0xff;
+	cmd[3] = addr & 0xff;
+	if (!send_cmd(em100->dev, cmd)) {
+		return 0;
+	}
+	int len = get_response(em100->dev, data, 256);
+
+	// FIXME are pages really effectively 255 bytes, or
+	// is this part of the format of the config page and
+	// thus the check for 255 needs to be moved to get_serialno()
+	if ((len == 256) && (data[0] == 255)) {
+		memcpy(blk, data, 256);
+		return 1;
+	}
+	return 0;
+}
+
+static int write_spi_flash_page(libusb_device_handle *dev, unsigned char *data, int address)
+{
+	int length = 256;
+	int actual;
+	int bytes_sent=0;
+	int bytes_left;
+	unsigned char cmd[16];
+	memset(cmd, 0, 16);
+	cmd[0] = 0x34; /* host-to-em100 eeprom data */
+	cmd[2] = (address >> 16) & 0xff;
+	cmd[3] = (address >> 8) & 0xff;
+	cmd[4] = address & 0xff;
+
+	if (!send_cmd(dev, cmd)) {
+		printf("error initiating host-to-em100 transfer.\n");
+		return 0;
+	}
+
+	while ( bytes_sent < length) {
+		actual = 0;
+
+		bytes_left = length - bytes_sent;
+
+		libusb_bulk_transfer(dev, 1 | LIBUSB_ENDPOINT_OUT,
+			data + bytes_sent, bytes_left, &actual, BULK_SEND_TIMEOUT);
+		bytes_sent += actual;
+		if (actual < bytes_left) {
+			printf("Tried sending %d bytes, sent %d\n", bytes_left, actual);
+			break;
+		}
+
+		printf("Sent %d bytes of %d\n", bytes_sent, length);
+	}
+
+	printf ("Transfer %s\n",bytes_sent == length ? "Succeeded" : "Failed");
+	return (bytes_sent == length);
+}
+
 /**
  * get_serialno: fetch device's serial number
  * @param em100: initialized em100 device structure
