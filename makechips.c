489da9f9de01b3c4a097d19ef06815f75f533f77
Stefan Reinauer <stefan.reinauer@coreboot.org>
2018-10-09 10:45:19 -0700

    em100: Fix copyright notices
    The Chromium OS Authors is not a well enough defined term outside
    of Chromium OS repositories. Clarify the copyright situation for
    the EM100 utility.
    
    Change-Id: I8c395b5bdaaea15329e893653f3f132e6f6b058c
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: https://review.coreboot.org/28981
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- makechips.c
+++ makechips.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Chromium OS Authors.
+ * Copyright 2012 Google Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- makechips.sh
+++ makechips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2012 The Chromium OS Authors.
+# Copyright 2012 Google Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
