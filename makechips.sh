72b9baf754a8471e9e287df7ee248420286e2767
Stefan Reinauer <stefan.reinauer@coreboot.org>
2019-11-26 17:07:10 -0800

    Clean up license headers
     - Google Inc. became Google LLC in 2017
     - Drop last paragraph of GPLv2
     - Fix GPLv2 header in hexdump.c
    
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Change-Id: I81375366181d59d835db43b413ec51f041e25488
    Reviewed-on: https://review.coreboot.org/c/em100/+/37259
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: David Hendricks <david.hendricks@gmail.com>

--- makechips.sh
+++ makechips.sh
@@ -11,10 +11,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-#
 
 URL="https://www.dediprog.com/download/save/78.msi"
 VURL="https://www.dediprog.com/download?productCategory=SPI+Flash+Solution&productName=EM100Pro-G2+SPI+NOR+Flash+Emulator&fileType=10"
--- makedpfw.c
+++ makedpfw.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google Inc.
+ * Copyright 2017 Google LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,10 +9,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc.
  */
 
 #include <stdio.h>
